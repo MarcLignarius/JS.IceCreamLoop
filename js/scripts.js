af9649b5670b7302fc9133660991a7a0eec3dd60
Marc Davies <marcdaviesriot@gmail.com>
2019-04-02 01:13:11 -0700

    add script to populate HTML list using an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-
+$(function() {
+  var flavors = ['white chocolate raspberry', 'malted moo shake', 'chocolate mudslide'];
+  var list = '<ul>'
+    flavors.forEach(function(flavor) {
+      list += '<li>'+ flavor + '</li>';
+    });
+    list += '</ul>';
+    document.getElementById("output").innerHTML = list;
+  });
